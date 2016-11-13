dfc33a0db1427d0863a0afbd2447ce0e2e8b30fb
aditay15084 <aditay15084@iiitd.ac.in>
2016-11-13 18:03:41 +0530

    LHUC is being implemented

--- lhuc.py
+++ lhuc.py
@@ -1,12 +1,39 @@
 # -*- coding: utf-8 -*-
 
+"""
+This tutorial introduces logistic regression using Theano and stochastic
+gradient descent.
 
-# References:
-# 
-    # - textbooks: "Pattern Recognition and Machine Learning" -
-                 # Christopher M. Bishop, section 4.3.2
+Logistic regression is a probabilistic, linear classifier. It is parametrized
+by a weight matrix :math:`W` and a bias vector :math:`b`. Classification is
+done by projecting data points onto a set of hyperplanes, the distance to
+which is used to determine a class membership probability.
 
-# """
+Mathematically, this can be written as:
+
+.. math::
+  P(Y=i|x, W,b) &= softmax_i(W x + b) \\
+                &= \frac {e^{W_i x + b_i}} {\sum_j e^{W_j x + b_j}}
+
+
+The output of the model or prediction is then done by taking the argmax of
+the vector whose i'th element is P(Y=i|x).
+
+.. math::
+
+  y_{pred} = argmax_i P(Y=i|x,W,b)
+
+
+This tutorial presents a stochastic gradient descent optimization method
+suitable for large datasets.
+
+
+References:
+
+    - textbooks: "Pattern Recognition and Machine Learning" -
+                 Christopher M. Bishop, section 4.3.2
+
+"""
 
 from __future__ import print_function
 
@@ -17,18 +44,41 @@ import gzip
 import os
 import sys
 import timeit
-
 import numpy
+# import numpy as np
 
 import theano
 import theano.tensor as T
 
 
 class LogisticRegression(object):
-    
+    """Multi-class Logistic Regression Class
+
+    The logistic regression is fully described by a weight matrix :math:`W`
+    and bias vector :math:`b`. Classification is done by projecting data
+    points onto a set of hyperplanes, the distance to which is used to
+    determine a class membership probability.
+    """
 
     def __init__(self, input, n_in, n_out):
-            self.W = theano.shared(
+        """ Initialize the parameters of the logistic regression
+
+        :type input: theano.tensor.TensorType
+        :param input: symbolic variable that describes the input of the
+                      architecture (one minibatch)
+
+        :type n_in: int
+        :param n_in: number of input units, the dimension of the space in
+                     which the datapoints lie
+
+        :type n_out: int
+        :param n_out: number of output units, the dimension of the space in
+                      which the labels lie
+
+        """
+        # start-snippet-1
+        # initialize with 0 the weights W as a matrix of shape (n_in, n_out)
+        self.W = theano.shared(
             value=numpy.zeros(
                 (n_in, n_out),
                 dtype=theano.config.floatX
@@ -45,7 +95,50 @@ class LogisticRegression(object):
             name='b',
             borrow=True
         )
+        self.L = theano.shared(
+            value=numpy.zeros(
+                (n_out,),
+                dtype=theano.config.floatX
+            ),
+            name='L',
+            borrow=True
+        )
+        self.p_y_given_x_lhuc = T.nnet.softmax(T.dot(input, self.W)*2*T.nnet.sigmoid(L) + self.b)
 
+        # symbolic description of how to compute prediction as class whose
+        # probability is maximal
+        self.y_pred_lhuc = T.argmax(self.p_y_given_x_lhuc, axis=1)
+        # end-snippet-1
+
+        # parameters of the model
+        self.params_lhuc = self.L
+        # self.W2 = theano.shared(
+            # value=numpy.zeros(
+                # (n_hidden, n_out),
+                # dtype=theano.config.floatX
+            # ),
+            # name='W2',
+            # borrow=True
+        # )
+
+        # self.b2 = theano.shared(
+            # value=numpy.zeros(
+                # (n_out,),
+                # dtype=theano.config.floatX
+            # ),
+            # name='b2',
+            # borrow=True
+        # )
+
+        # symbolic expression for computing the matrix of class-membership
+        # probabilities
+        # Where:
+        # W is a matrix where column-k represent the separation hyperplane for
+        # class-k
+        # x is a matrix where row-j  represents input training sample-j
+        # b is a vector where element-k represent the free parameter of
+        # hyperplane-k
+        # self.h1 = T.dot(input, self.W) + self.b
         self.p_y_given_x = T.nnet.softmax(T.dot(input, self.W) + self.b)
 
         # symbolic description of how to compute prediction as class whose
@@ -60,24 +153,105 @@ class LogisticRegression(object):
         self.input = input
 
     def negative_log_likelihood(self, y):
+        """Return the mean of the negative log-likelihood of the prediction
+        of this model under a given target distribution.
+
+        .. math::
+
+            \frac{1}{|\mathcal{D}|} \mathcal{L} (\theta=\{W,b\}, \mathcal{D}) =
+            \frac{1}{|\mathcal{D}|} \sum_{i=0}^{|\mathcal{D}|}
+                \log(P(Y=y^{(i)}|x^{(i)}, W,b)) \\
+            \ell (\theta=\{W,b\}, \mathcal{D})
+
+        :type y: theano.tensor.TensorType
+        :param y: corresponds to a vector that gives for each example the
+                  correct label
+
+        Note: we use the mean instead of the sum so that
+              the learning rate is less dependent on the batch size
+        """
+        # start-snippet-2
+        # y.shape[0] is (symbolically) the number of rows in y, i.e.,
+        # number of examples (call it n) in the minibatch
+        # T.arange(y.shape[0]) is a symbolic vector which will contain
+        # [0,1,2,... n-1] T.log(self.p_y_given_x) is a matrix of
+        # Log-Probabilities (call it LP) with one row per example and
+        # one column per class LP[T.arange(y.shape[0]),y] is a vector
+        # v containing [LP[0,y[0]], LP[1,y[1]], LP[2,y[2]], ...,
+        # LP[n-1,y[n-1]]] and T.mean(LP[T.arange(y.shape[0]),y]) is
+        # the mean (across minibatch examples) of the elements in v,
+        # i.e., the mean log-likelihood across the minibatch.
         return -T.mean(T.log(self.p_y_given_x)[T.arange(y.shape[0]), y])
         # end-snippet-2
+    def costLHUC(self,y):
+    	return -T.mean(T.log(self.p_y_given_x_lhuc)[T.arange(y.shape[0]),y])
+
 
     def errors(self, y):
-        
+        """Return a float representing the number of errors in the minibatch
+        over the total number of examples of the minibatch ; zero one
+        loss over the size of the minibatch
+
+        :type y: theano.tensor.TensorType
+        :param y: corresponds to a vector that gives for each example the
+                  correct label
+        """
+
+        # check if y has same dimension of y_pred
         if y.ndim != self.y_pred.ndim:
             raise TypeError(
                 'y should have the same shape as self.y_pred',
                 ('y', y.type, 'y_pred', self.y_pred.type)
             )
+        # check if y is of the correct datatype
         if y.dtype.startswith('int'):
-            
+            # the T.neq operator returns a vector of 0s and 1s, where 1
+            # represents a mistake in prediction
             return T.mean(T.neq(self.y_pred, y))
         else:
             raise NotImplementedError()
+    # def addLHUC(self):
 
+        # self.L = theano.shared(
+            # value=numpy.zeros(
+                # (n_out,),
+                # dtype=theano.config.floatX
+            # ),
+            # name='L',
+            # borrow=True
+        # )
+        # self.p_y_given_x_lhuc = T.nnet.softmax(T.dot(input, self.W)*2*T.nnet.sigmoid(L) + self.b)
+
+        # symbolic description of how to compute prediction as class whose
+        # probability is maximal
+        # self.y_pred_lhuc = T.argmax(self.p_y_given_x_lhuc, axis=1)
+        # end-snippet-1
+
+        # parameters of the model
+        # self.params_lhuc = self.L
+
+        # initialize the biases b as a vector of n_out 0s
+        # self.b = theano.shared(
+            # value=numpy.zeros(
+                # (n_out,),
+                # dtype=theano.config.floatX
+            # ),
+            # name='b',
+            # borrow=True
+        # )
 
 def load_data(dataset):
+    ''' Loads the dataset
+
+    :type dataset: string
+    :param dataset: the path to the dataset (here MNIST)
+    '''
+
+    #############
+    # LOAD DATA #
+    #############
+
+    # Download the MNIST dataset if it is not present
     data_dir, data_file = os.path.split(dataset)
     if data_dir == "" and not os.path.isfile(dataset):
         # Check if dataset is in the data directory.
@@ -106,16 +280,40 @@ def load_data(dataset):
             train_set, valid_set, test_set = pickle.load(f, encoding='latin1')
         except:
             train_set, valid_set, test_set = pickle.load(f)
+ 	test_setx, test_sety = test_set    
+ 	test_setx_noisy = test_setx + 0.2*numpy.random.random_sample(numpy.shape(test_setx))
+
+	test_set = (test_setx_noisy, test_sety) 
+    # train_set, valid_set, test_set format: tuple(input, target)
+    # input is a numpy.ndarray of 2 dimensions (a matrix)
+    # where each row corresponds to an example. target is a
+    # numpy.ndarray of 1 dimension (vector) that has the same length as
+    # the number of rows in the input. It should give the target
+    # to the example with the same index in the input.
 
     def shared_dataset(data_xy, borrow=True):
-                data_x, data_y = data_xy
+        """ Function that loads the dataset into shared variables
+
+        The reason we store our dataset in shared variables is to allow
+        Theano to copy it into the GPU memory (when code is run on GPU).
+        Since copying data into the GPU is slow, copying a minibatch everytime
+        is needed (the default behaviour if the data is not in a shared
+        variable) would lead to a large decrease in performance.
+        """
+        data_x, data_y = data_xy
         shared_x = theano.shared(numpy.asarray(data_x,
                                                dtype=theano.config.floatX),
                                  borrow=borrow)
         shared_y = theano.shared(numpy.asarray(data_y,
                                                dtype=theano.config.floatX),
                                  borrow=borrow)
-        
+        # When storing data on the GPU it has to be stored as floats
+        # therefore we will store the labels as ``floatX`` as well
+        # (``shared_y`` does exactly that). But during our computations
+        # we need them as ints (we use labels as index, and if they are
+        # floats it doesn't make sense) therefore instead of returning
+        # ``shared_y`` we will have to cast it to int. This little hack
+        # lets ous get around this issue
         return shared_x, T.cast(shared_y, 'int32')
 
     test_set_x, test_set_y = shared_dataset(test_set)
@@ -130,13 +328,35 @@ def load_data(dataset):
 def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
                            dataset='mnist.pkl.gz',
                            batch_size=600):
-    
+    """
+    Demonstrate stochastic gradient descent optimization of a log-linear
+    model
+
+    This is demonstrated on MNIST.
+
+    :type learning_rate: float
+    :param learning_rate: learning rate used (factor for the stochastic
+                          gradient)
+
+    :type n_epochs: int
+    :param n_epochs: maximal number of epochs to run the optimizer
+
+    :type dataset: string
+    :param dataset: the path of the MNIST dataset file from
+                 http://www.iro.umontreal.ca/~lisa/deep/data/mnist/mnist.pkl.gz
+
+    """
     datasets = load_data(dataset)
 
     train_set_x, train_set_y = datasets[0]
     valid_set_x, valid_set_y = datasets[1]
     test_set_x, test_set_y = datasets[2]
-
+    # shape = numpy.shape(test_set_x)
+    # r = shape[0]
+    # c = shape[1]
+    # print(r)
+    # print(c)
+    # test_set_x = test_set_x + numpy.random.randn(r,c)
     # compute number of minibatches for training, validation and testing
     n_train_batches = train_set_x.get_value(borrow=True).shape[0] // batch_size
     n_valid_batches = valid_set_x.get_value(borrow=True).shape[0] // batch_size
@@ -165,8 +385,11 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
 
     # compiling a Theano function that computes the mistakes that are made by
     # the model on a minibatch
+    g_L = T.grad(cost=classifier.costLHUC(self.y), wrt = classifier.L)
+    updatesLHUC = [(classifier.L, classifier.L - learning_rate * g_L)]
     test_model = theano.function(
         inputs=[index],
+        updates = updatesLHUC,
         outputs=classifier.errors(y),
         givens={
             x: test_set_x[index * batch_size: (index + 1) * batch_size],
@@ -186,12 +409,15 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
     # compute the gradient of cost with respect to theta = (W,b)
     g_W = T.grad(cost=cost, wrt=classifier.W)
     g_b = T.grad(cost=cost, wrt=classifier.b)
-
+    # g_W2 = T.grad(cost=cost, wrt=classifier.W2)
+    # g_b2 = T.grad(cost=cost, wrt=classifier.b2)
     # start-snippet-3
     # specify how to update the parameters of the model as a list of
     # (variable, update expression) pairs.
     updates = [(classifier.W, classifier.W - learning_rate * g_W),
                (classifier.b, classifier.b - learning_rate * g_b)]
+               # (classifier.W2, classifier.W2 - learning_rate * g_W2),
+               # (classifier.b2, classifier.b2 - learning_rate * g_b2)]
 
     # compiling a Theano function `train_model` that returns the cost, but in
     # the same time updates the parameter of the model based on the rules
@@ -302,7 +528,12 @@ def sgd_optimization_mnist(learning_rate=0.13, n_epochs=1000,
            os.path.split(__file__)[1] +
            ' ran for %.1fs' % ((end_time - start_time))), file=sys.stderr)
 
-
+    nEpochs_Lhuc = 20
+    epoch = 0
+    while (epoch < nEpochs_Lhuc):
+    	epoch = epoch + 1
+    		
+    	# pass
 def predict():
     """
     An example of how to load a trained model and use it
